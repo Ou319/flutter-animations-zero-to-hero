8b75832038af220dcd2252ba9376adb16d8ad7de
outmane el ouafa <outmaneelouafa241@gmail.com>
2025-03-21 14:33:16 +0100

    lecture 1

---
+++ animation_core#1/animation_core1/lib/luctures/lecture3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lecture3 extends StatefulWidget {
+  const Lecture3({super.key});
+
+  @override
+  State<Lecture3> createState() => _Lecture3State();
+}
+
+class _Lecture3State extends State<Lecture3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
