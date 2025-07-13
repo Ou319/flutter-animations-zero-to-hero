db4ab1eb3104a0ee8068cac77848a802ddf9355f
Your Name <outmaneelouafa241@gmail.com>
2025-07-14 02:54:10 +0300

    animate list

---
+++ animation_core1/lib/luctures/lecture11.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class animationListed extends StatefulWidget {
+  const animationListed({super.key});
+
+  @override
+  State<animationListed> createState() => _animationListedState();
+}
+
+class _animationListedState extends State<animationListed> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
